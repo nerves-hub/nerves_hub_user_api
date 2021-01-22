03ff0f419b85c652fd5a69c81dadb15059a39c10
Jean Parpaillon <jean.parpaillon@free.fr>
2021-01-22 17:25:58 +0100

    Ignore invalid cert files from cert dir
    Closes #26

--- lib/nerves_hub_user_api/api.ex
+++ lib/nerves_hub_user_api/api.ex
@@ -147,7 +147,12 @@ defmodule NervesHubUserAPI.API do
     ca_cert_path
     |> File.ls!()
     |> Enum.map(&File.read!(Path.join(ca_cert_path, &1)))
-    |> Enum.map(&Certificate.from_pem!/1)
+    |> Enum.reduce([], fn bin, acc ->
+      case Certificate.from_pem(bin) do
+        {:ok, cert} -> [cert | acc]
+        {:error, _} -> acc
+      end
+    end)
     |> Enum.map(&Certificate.to_der/1)
   end
 
